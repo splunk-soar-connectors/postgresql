9547ec8b0d181b8b9d554bb47a2f80e45a5f0fba
mishalp <mishalp@splunk.com>
2021-01-07 17:19:45 +0530

    PAPP-13290 : Initial commit for 2to3 conversion

--- postgresql_view.py
+++ postgresql_view.py
@@ -1,16 +1,13 @@
 # File: postgresql_view.py
-# Copyright (c) 2017-2019 Splunk Inc.
+# Copyright (c) 2017-2021 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
 
-from django.http import HttpResponse
-import json
-
-
 def display_query_results(provides, all_results, context):
 
     headers = []
+    adjusted_names = {}
 
     headers_set = set()
     for summary, action_results in all_results:
@@ -18,45 +15,25 @@ def display_query_results(provides, all_results, context):
             header_data = result.get_data()
 
     if header_data:
-        headers += header_data[0].keys()
+        headers += list(header_data[0].keys())
 
     if not headers_set:
         headers_set.update(headers)
     headers = sorted(headers_set)
 
-    context['ajax'] = True
-    if 'start' not in context['QS']:
-        context['headers'] = headers
-        return '/widgets/generic_table.html'
+    context['results'] = results = []
+    context['headers'] = headers
 
-    adjusted_names = {}
-
-    start = int(context['QS']['start'][0])
-    length = int(context['QS'].get('length', ['5'])[0])
-    end = start + length
-    cur_pos = 0
-    rows = []
-    total = 0
     for summary, action_results in all_results:
         for result in action_results:
+            table = {}
+            table['data'] = table_data = []
             data = result.get_data()
-            total += len(data)
             for item in data:
-                cur_pos += 1
-                if (cur_pos - 1) < start:
-                    continue
-                if (cur_pos - 1) >= end:
-                    break
                 row = []
-
                 for h in headers:
                     row.append({ 'value': item.get(adjusted_names.get(h, h)) })
-                rows.append(row)
+                table_data.append(row)
+            results.append(table)
 
-    content = {
-        "data": rows,
-        "recordsTotal": total,
-        "recordsFiltered": total,
-    }
-
-    return HttpResponse(json.dumps(content), content_type='text/javascript')
+    return 'phantom_multiple_actions.html'
