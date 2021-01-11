ca18edafddf114895ba99586603b10f93232336a
ngaglani <ngaglani@splunk.com>
2021-01-11 12:41:52 +0530

    Updating the custom view file for run query action

--- postgresql_view.py
+++ postgresql_view.py
@@ -6,34 +6,27 @@
 
 def display_query_results(provides, all_results, context):
 
-    headers = []
+    context['results'] = results = []
+
     adjusted_names = {}
 
-    headers_set = set()
     for summary, action_results in all_results:
         for result in action_results:
-            header_data = result.get_data()
-
-    if header_data:
-        headers += list(header_data[0].keys())
-
-    if not headers_set:
-        headers_set.update(headers)
-    headers = sorted(headers_set)
-
-    context['results'] = results = []
-    context['headers'] = headers
-
-    for summary, action_results in all_results:
-        for result in action_results:
-            table = {}
-            table['data'] = table_data = []
+            headers_set = set()
+            table = dict()
+            table['data'] = rows = []
             data = result.get_data()
+            if data:
+                headers_set.update(data[0].keys())
+            headers = sorted(headers_set)
+            table['headers'] = headers
+
             for item in data:
                 row = []
+
                 for h in headers:
                     row.append({ 'value': item.get(adjusted_names.get(h, h)) })
-                table_data.append(row)
+                rows.append(row)
             results.append(table)
 
     return 'postgresql_run_query.html'
